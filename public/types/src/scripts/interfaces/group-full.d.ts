86ceb975fdb7401d34967819e73ad5018518ccac
Ingo <thewatcher@watchersnet.de>
2025-07-10 12:29:01 +0200

    fixes

---
+++ public/types/src/scripts/interfaces/group-full.d.ts
@@ -0,0 +1,11 @@
+import { ChoiceFull } from './choice-full';
+export interface GroupFull {
+    id: number;
+    active: boolean;
+    disabled: boolean;
+    label?: string;
+    value?: string;
+    element?: HTMLOptGroupElement;
+    groupEl?: HTMLElement;
+    choices: ChoiceFull[];
+}
